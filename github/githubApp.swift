db1a3d23ccc4fbd334d72be8b6b429193bc2a3f1
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-05-19 00:17:38 -0300

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Juan Carlos Carbajal Ipenza on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
