840fedb2149b9b3f3dbcc611d162db6ffc31d50b
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2023-05-19 01:41:20 -0300

    Added colors, fonts, logo, localizations, Accent Color and App icon

--- Modules/Sources/Core/Core.swift
+++
@@ -1,6 +0,0 @@
-public struct Core {
-    public private(set) var text: String = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Modules/Sources/Core/Extensions/Bundle+Extensions.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public extension Bundle {
+    static func getCoreBundle() -> Bundle {
+        Bundle.module
+    }
+}
